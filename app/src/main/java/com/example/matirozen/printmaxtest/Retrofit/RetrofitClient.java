104f17f6849ace5a066f24d9435e2f0b79d66d71
Francisco Zuccalá <francisco.zuccala@despegar.com>
2018-09-21 21:58:45 -0300

    Created new client

--- app/src/main/java/com/example/matirozen/printmaxtest/Retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/matirozen/printmaxtest/Retrofit/RetrofitClient.java
@@ -1,18 +1,52 @@
 package com.example.matirozen.printmaxtest.Retrofit;
 
+import com.example.matirozen.printmaxtest.BuildConfig;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class RetrofitClient {
-    private static Retrofit retrofit=null;
+
+    private static Retrofit retrofit = null;
 
     public static Retrofit getClient(String baseUrl){
         if(retrofit == null){
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(createClient())
+                    .addConverterFactory(JacksonConverterFactory.create(createMapper()))
                     .build();
         }
         return retrofit;
+    }
+
+    private static ObjectMapper createMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        return mapper;
+    }
+
+    private static OkHttpClient createClient(){
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(15, TimeUnit.SECONDS);
+        builder.readTimeout(30, TimeUnit.SECONDS);
+        if (BuildConfig.DEBUG){
+            builder.addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY));
+        }
+        return builder.build();
     }
 }
